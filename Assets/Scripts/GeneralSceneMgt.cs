ce4b358cac46f4448bca81de92a6ecf3bfb50240
SylvainC <sylvain.capart@hotmail.fr>
2020-01-31 23:51:29 +0100

    Scene Mgt change

--- Assets/Scripts/GeneralSceneMgt.cs
+++ Assets/Scripts/GeneralSceneMgt.cs
@@ -4,32 +4,8 @@ using UnityEngine.SceneManagement;
 public class GeneralSceneMgt : MonoBehaviour
 {
     public enum SceneIndex {MENU, GAME};
-    public enum GameMode { NORMAL, HARD };
 
     public static GeneralSceneMgt instance;
-
-    private const float EPSILON = 0.01f;
-
-    public GameMode m_GameMode;
-
-    private bool m_IsMenuPlayedOnce;
-
-    public bool IsMenuPlayedOnce
-    {
-        get
-        {
-            return m_IsMenuPlayedOnce;
-        }
-
-        set
-        {
-            m_IsMenuPlayedOnce = value;
-        }
-    }
-
-    public delegate void GenScnMgtDlg();
-    public static event GenScnMgtDlg OnMenuInit;
-    public static event GenScnMgtDlg OnMenuAlreadyInit;
 
     private void Awake()
     {
@@ -49,22 +25,6 @@ public class GeneralSceneMgt : MonoBehaviour
         }
     }
 
-    private void Start()
-    {
-        if (!IsMenuPlayedOnce)
-        {
-            if (OnMenuInit != null)
-                OnMenuInit();
-            IsMenuPlayedOnce = true;
-        }
-        else
-        {
-            if (OnMenuAlreadyInit != null)
-                OnMenuAlreadyInit();
-        }
-    }
-
-
     public void GoToScene(SceneIndex scene_index)
     {
         SceneManager.LoadScene((int)scene_index);
@@ -72,12 +32,9 @@ public class GeneralSceneMgt : MonoBehaviour
 
     public void GoToMenu()
     {
-        if (System.Math.Abs(Time.timeScale - 1f) > EPSILON)
-            Time.timeScale = 1f;
+        //if (System.Math.Abs(Time.timeScale - 1f) > EPSILON)
+         //   Time.timeScale = 1f;
 
-        AudioManager.instance.StopSound(AudioManager.instance.MainSound.name);
-        AudioManager.instance.PlaySound("Guitar");
-        AudioManager.instance.MainSound = AudioManager.instance.GetSound("Guitar");
         SceneManager.LoadScene((int)SceneIndex.MENU);
 
 
@@ -85,26 +42,10 @@ public class GeneralSceneMgt : MonoBehaviour
 
     public void GoToGameNormal()
     {
-        m_GameMode = GameMode.NORMAL;
 
-        if (System.Math.Abs(Time.timeScale - 1f) > EPSILON)
-            Time.timeScale = 1f;
-        AudioManager.instance.StopSound("Guitar");
-        AudioManager.instance.PlaySound("Music");
-        AudioManager.instance.MainSound = AudioManager.instance.GetSound("Music");
-        SceneManager.LoadScene((int)SceneIndex.GAME);
+  //      if (System.Math.Abs(Time.timeScale - 1f) > EPSILON)
+  //          Time.timeScale = 1f;
 
-    }
-
-    public void GoToGameHard()
-    {
-        m_GameMode = GameMode.HARD;
-
-        if (System.Math.Abs(Time.timeScale - 1f) > EPSILON)
-            Time.timeScale = 1f;
-        AudioManager.instance.StopSound("Guitar");
-        AudioManager.instance.PlaySound("Music");
-        AudioManager.instance.MainSound = AudioManager.instance.GetSound("Music");
         SceneManager.LoadScene((int)SceneIndex.GAME);
 
     }
