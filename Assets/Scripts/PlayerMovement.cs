46c685b4264e69be9edc095f5a7734b481423ddc
godpepper <arnaud.dumont@posteo.net>
2020-02-01 13:41:27 +0100

    Smoothing motion + Player control made into Character Controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,56 +4,57 @@ using System;
 public class PlayerMovement : MonoBehaviour {
 
 	// This is a reference to the Rigidbody component called "rb"
-	public Rigidbody2D rb;
-	
-	public float forwardForce = 10f;	// Variable that determines the forward force
-	public float upForceMoving = 10f;  // Variable that determines the sideways force
-	public float upForceJumping = 50f;
-	private Vector2 force;
+
+	public Vector2 movement;
 
 	void Start()
 	{
 	}
 	// We marked this as "Fixed"Update because we
 	// are using it to mess with physics.
-	void FixedUpdate ()
+	
+	void Update()
 	{
-		// Add a forward force		
-
-		if (Input.GetAxisRaw("Horizontal") == 1)	// If the player is pressing the "d" key
-		{
-			MoveForward();
-		}
-
-		if (Input.GetAxisRaw("Horizontal") == -1)  // If the player is pressing the "a" key
-		{
-			MoveBackWards();
-		}
-
-		if (Input.GetAxisRaw("Vertical") == 1)  // If the player is pressing the "a" key
-		{
-			Jump();
-		}
+		movement.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 	}
-	void MoveForward()
-	{
-		force.Set(forwardForce, upForceMoving);
-		rb.AddForce(force);
-	}
+	// void FixedUpdate ()
+	// {
+	// 	// Add a forward force		
 
-	void MoveBackWards()
-	{
-		force.Set(-forwardForce, upForceMoving);
-		rb.AddForce(force);
-	}
+	// 	if (Input.GetAxisRaw("Horizontal") == 1)	// If the player is pressing the "d" key
+	// 	{
+	// 		MoveForward();
+	// 	}
 
-	void Jump()
-	{
-		if (rb.transform.position.y <1)
-		{
-			force.Set(0, upForceJumping);
-			rb.AddForce(force);
-		}
-	}
+	// 	if (Input.GetAxisRaw("Horizontal") == -1)  // If the player is pressing the "a" key
+	// 	{
+	// 		MoveBackWards();
+	// 	}
+
+	// 	if (Input.GetAxisRaw("Vertical") == 1)  // If the player is pressing the "a" key
+	// 	{
+	// 		Jump();
+	// 	}
+	// }
+	// void MoveForward()
+	// {
+	// 	force.Set(forwardForce, upForceMoving);
+	// 	rb.AddForce(force);
+	// }
+
+	// void MoveBackWards()
+	// {
+	// 	force.Set(-forwardForce, upForceMoving);
+	// 	rb.AddForce(force);
+	// }
+
+	// void Jump()
+	// {
+	// 	if (rb.transform.position.y <1)
+	// 	{
+	// 		force.Set(0, upForceJumping);
+	// 		rb.AddForce(force);
+	// 	}
+	// }
 }
 
